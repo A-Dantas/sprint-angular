9eccc9a7d5f87c2ae9cee9e426a7eb602c8fd5e9
PyDantas Dev <adevairdantas@gmail.com>
2025-11-04 11:39:31 -0300

    Contrução parcial da pag. dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent},
-    { path: 'home', component: HomeComponent}
+    { path: 'home', component: HomeComponent},
+    { path: 'vehicle', component: DashboardComponent}
 ];
