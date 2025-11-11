abf389883027a973a14249c9706a46dd1f187e29
PyDantas Dev <adevairdantas@gmail.com>
2025-11-11 10:51:58 -0300

    Ajustes no menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  menuAberto: boolean = false;
+
+  constructor() {
+  }
+
+  toggleMenu() {
+    this.menuAberto = !this.menuAberto;
+  }
   
 }
